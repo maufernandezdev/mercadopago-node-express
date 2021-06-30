047771a0c6b6913bf96579a02e75b43d7b380f99
Mauricio Fernandez <mfernandezuk@hotmail.com>
2021-06-29 21:06:44 -0300

    AJAX challenge

--- js/Lib_login_customer.js
+++ js/Lib_login_customer.js
@@ -2,39 +2,34 @@
 
 // ejemplo de array de clientes 
 
-let Customers = 
-[
-    {
-        name:"Maria",
-        table:"7",
+let Customers = [{
+        name: "Maria",
+        table: "7",
     },
     {
-        name:"Lucia",
-        table:"2",
+        name: "Lucia",
+        table: "2",
     },
     {
-        name:"Pablo",
-        table:"1",
+        name: "Pablo",
+        table: "1",
     },
     {
-        name:"Ian",
-        table:"3",
+        name: "Ian",
+        table: "3",
     }
-           
+
 ];
 let lastCustomer = "";
 
-var Functions =
-{
+var Functions = {
     //set last customer
-    setLastCustomer : function ()
-    {
+    setLastCustomer: function() {
         lastCustomer = Customers[Customers.length - 1].toString();
     },
-    
+
     // return last customer entered
-    getLastCustomer:function ()
-    {
+    getLastCustomer: function() {
         return lastCustomer;
     },
     // sortArray:function()
@@ -50,9 +45,9 @@ var Functions =
     //         {
     //             return 1;
     //         }
-    
+
     //         return 0;
-        
+
     //     });
     // },
 
@@ -60,38 +55,29 @@ var Functions =
 
 
 // adding new customers
-function addCustomer()
-{   
+function addCustomer() {
     var newCustomer = document.getElementById('name').value;
     var table = document.getElementById('table').value;
-    addNewCustomer(newCustomer,table);
+    addNewCustomer(newCustomer, table);
 
 }
 
-function addCustomerByEnter(e)
-{
-    if(e.keyCode === 13)
-    {
+function addCustomerByEnter(e) {
+    if (e.keyCode === 13) {
         var newCustomer = document.getElementById('name').value;
         var table = document.getElementById('table').value;
-        addNewCustomer(newCustomer,table);
+        addNewCustomer(newCustomer, table);
     }
 }
 
 
-function addNewCustomer(newCustomer,table)
-{
-    Customers.push({name:newCustomer,table:table} ); // new customer is added to array
+function addNewCustomer(newCustomer, table) {
+    Customers.push({ name: newCustomer, table: table }); // new customer is added to array
     Functions.setLastCustomer(); // set last customer variable 
     //Functions.sortArray(); //array order with sort method
     console.log(Customers);
     console.log(Functions.getLastCustomer());
-    document.getElementById('name').value = ""; // set name field to empty
-    document.getElementById('table').value = "";
-    window.location.href = "customer_order.html"; // redirect to customer order 
+    // document.getElementById('name').value = ""; // set name field to empty
+    // document.getElementById('table').value = "";
+    //window.location.href = "customer_order.html"; // redirect to customer order 
 }
-
-
-    
-
-
